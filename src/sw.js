4fad78181b24c33b1232e8c1dc18e0c36eb77881
ragingwind <ragingwind@gmail.com>
2016-12-05 14:18:04 +0900

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,14 @@
-// service-worker.js
+// sw.js
 
-var CACHE_NAME = 'userid.github.id-cache-v1';
+var CACHE_NAME = 'pwa-workshop.github.id-namp-card-cache-v1';
 var urlsToCache = [
-	'/',
+	'/namp-card'
 ];
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function(cache) {
-			console.log('Opened cache');
+			console.log(`Opened cache for namp-card ${new Date()}`);
 			return cache.addAll(urlsToCache);
 		})
 	);
