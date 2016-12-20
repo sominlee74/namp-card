3f08397e4b9e16a09e23c785526ff25c11d83bad
Somin Lee <kunoctus@gmail.com>
2016-12-20 20:54:06 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
 // sw.js
 
-var CACHE_NAME = 'pwa-workshop.github.id-namp-card-cache-v1';
+//var CACHE_NAME = 'pwa-workshop.github.id-namp-card-cache-v1';
+var CACHE_NAME = 'myCache-Simon.id-namp-card-cache-v1';
 var urlsToCache = [
-	'/namp-card'
+	'/namp-card',
+	'/index.html',
+	'/manifest.json',
+	'/user.png'
 ];
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function(cache) {
-			console.log(`Opened cache for nampcard ${new Date()}`);
-			return cache.addAll(urlsToCache);
+			console.log(`Opened cache for namp-card ${new Date()}`);
+			return caches.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.waitUntil(
+		caches.match(event.request.url).then(function(res) {
+			return res || fetch(event.request.url);
 		})
 	);
 });
